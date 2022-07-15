f928ba8518b0c5f0f5c453eb958834d78f15179c
eunjeong-97 <eoss7777@naver.com>
2022-07-15 19:09:08 +0900

    3.11 FlatList(2)
    - ScrollView 안에 수직방향의 FlatList를 사용하고 싶은경우
    - 기존의 ScrollView인 Container안에 수직방향의 FlatList인 Array컴포넌트를 넣고 싶다고 할때
    - Container를 Array컴포넌트의 data를 받는 FlatList로 만들고
    - Array컴포넌트위에 잇는건 ListHeaderComponent에 넣으면 된다 이때
      사이즈가 엄청 커도 Fragment로 감싸서 하나의 컴포넌트로 넣어주면됨
    - FlatList에서 새로고침은 refreshControl prop이나 RefreshControl 컴포넌트는 필요없이
      refreshing, onRefresh만 prop으로 전달하면 된다

--- src/screen/Movies.tsx
+++ src/screen/Movies.tsx
@@ -70,85 +70,41 @@ const Movies: React.FC<NativeStackScreenProps<any, "Movies">> = () => {
       <ActivityIndicator />
     </Loading>
   ) : (
-    <Container refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}>
-      <Swiper horizontal loop autoplay autoplayTimeout={2} showsButtons={false} showsPagination={false} containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4, marginBottom: 25 }}>
-        {nowPlayingMovies.map((movie) => {
-          return movie.vote_average > 0 ? <Slide key={movie.id} id={movie.id} backdropPath={movie.backdrop_path} posterPath={movie.poster_path} originalTitle={movie.original_title} voteAverage={movie.vote_average} overview={movie.overview} /> : null;
-        })}
-      </Swiper>
-      <ListWrap>
-        <ListTitle>Trending Movies</ListTitle>
-        {/* <TrendingScroll horizontal showsHorizontalScrollIndicator={false} contentContainerStyle={{ paddingHorizontal: 30 }}>
-          {trending.map((movie) => (
-            <Movie key={movie.id} posterPath={movie.poster_path} title={movie.original_title} voteAverage={movie.vote_average} wrapperStyle={{ marginLeft: 10 }} />
-          ))}
-        </TrendingScroll> */}
-        {/*renderItem에서 item은 배열 형태의 data에서 각각의 요소를 가리킨다
-          VirtualizedList는 같은 방향의 ScrollView 안에 감싸져있으면 안된다
-          즉 수직방향의 FlatList를 수직방향의 ScrollView 안에 넣을 수 없다는 뜻이다 
-          그리고 모든 속성을 받기 때문에 ScrollView에서 사용하던 prop을 사용할수도 잇다
-        */}
-        <FlatList
-          data={trending}
-          style={{ marginTop: 15 }}
-          ItemSeparatorComponent={() => <View style={{ width: 15 }} />}
-          renderItem={({ item }) => (
-            <Movie
-              posterPath={item.poster_path}
-              title={item.original_title}
-              voteAverage={item.vote_average}
-              //  wrapperStyle={{ marginLeft: 10 }}
+    <FlatList
+      data={upcoming}
+      ListHeaderComponent={() => (
+        <>
+          <Swiper horizontal loop autoplay autoplayTimeout={2} showsButtons={false} showsPagination={false} containerStyle={{ width: "100%", height: SCREEN_HEIGHT / 4, marginBottom: 25 }}>
+            {nowPlayingMovies.map((movie) => {
+              return movie.vote_average > 0 ? <Slide key={movie.id} id={movie.id} backdropPath={movie.backdrop_path} posterPath={movie.poster_path} originalTitle={movie.original_title} voteAverage={movie.vote_average} overview={movie.overview} /> : null;
+            })}
+          </Swiper>
+          <ListWrap>
+            <ListTitle>Trending Movies</ListTitle>
+            <FlatList
+              data={trending}
+              style={{ marginTop: 15 }}
+              ItemSeparatorComponent={() => <View style={{ width: 15 }} />}
+              renderItem={({ item }) => <Movie posterPath={item.poster_path} title={item.original_title} voteAverage={item.vote_average} />}
+              horizontal
+              showsHorizontalScrollIndicator={false}
+              contentContainerStyle={{ paddingHorizontal: 30 }}
             />
-          )}
-          horizontal
-          showsHorizontalScrollIndicator={false}
-          contentContainerStyle={{ paddingHorizontal: 30 }}
-        />
-      </ListWrap>
-      <ListWrap>
-        <ListTitle>Comming Soon</ListTitle>
-        {/* {upcoming.map((movie) => (
-          <Movie key={movie.id} posterPath={movie.poster_path} title={movie.original_title} direction="row" overview={movie.overview} wrapperStyle={{ paddingHorizontal: 30, paddingTop: 15 }} textWrapSize={60} date={movie.release_date} />
-        ))} */}
-        {/*
-        지금은 Movie컴포넌트 자체에서 marginTop:15를 주긴 했지만 Movie 컴포넌트 자체가 간격을 가지는 것이 아니라 리스트로 존재할때 간격이 15가 필요한 것이기 때문에 Movie에 간격을 지정하는것이 아니라 FlatList에서 item 사이에는 간격 15px을 render해야한다는걸 알아야 한다
-      
-        ScrollView는 이렇게 지정하는 것이 없기 때문에 margin을 통해 수동으로 각각의 컴포넌트를 밀어줬지만 
-        FlatList에서는 우리가 seperator를 render할 수 잇게 해줫다
-        ✨ 컴포넌트를 만들어서 style을 입힐때에는 어떤 속성이 정말 이 컴포넌트에 필요한지 어떤 속성이 외부 레이아웃에 필요한건지 구분해야 한다
-
-        따라서 FlatList에서 seperator를 render하도록 만든 속성이 ItemSeparatorComponent이다 
-        ItemSeparatorComponent는 item 사이사이에 컴포넌트들을 render해준다
-        그리고 FlatList에서 맨 끝에 존재하는요소에는 ItemSeparatorComponent를 넣지 않는다
-        */}
-        <FlatList
-          data={upcoming}
-          /* 숫자타입의 virtualizedCell.cellkey는 안된다는 에러가 발생하는 이유는
-          ReactJS에서 map으로 여러 요소를 render하면 key가 있어야 하는데 FlatList에서는 어떤걸 key로 할지 지정을 못하기 때문이다
-          그래서 각각의 renderItem에 어떤 key를 넣어줘야하는지 FlatList에게 알려주는 속성으로     keyExtractor에 함수를 전달하면 된다 
-          이 함수는 리스트의 item을 받아와서 item의 어떤부분을 key로 삼을건지 반환해주면된다
-          */
-          keyExtractor={(item) => item.id + ""}
-          ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
-          renderItem={({ item }) => (
-            <Movie
-              posterPath={item.poster_path}
-              title={item.original_title}
-              direction="row"
-              overview={item.overview}
-              wrapperStyle={{ paddingHorizontal: 30 }}
-              textWrapSize={60}
-              date={item.release_date}
-              // wrapperStyle={{ paddingHorizontal: 30, paddingTop:15 }}
-            />
-          )}
-        />
-      </ListWrap>
-    </Container>
+          </ListWrap>
+          <ListWrap>
+            <ListTitle>Comming Soon</ListTitle>
+          </ListWrap>
+        </>
+      )}
+      keyExtractor={(item) => item.id + ""}
+      ItemSeparatorComponent={() => <View style={{ height: 15 }} />}
+      renderItem={({ item }) => <Movie posterPath={item.poster_path} title={item.original_title} direction="row" overview={item.overview} wrapperStyle={{ paddingHorizontal: 30 }} textWrapSize={60} date={item.release_date} />}
+      // refreshControl={<RefreshControl refreshing={refresh} onRefresh={onRefresh} />}
+      refreshing={refresh}
+      onRefresh={onRefresh}
+    />
   );
 };
-
-const Container = styled.ScrollView``;
 
 const Loading = styled.View`
   flex: 1;
@@ -167,8 +123,14 @@ const ListTitle = styled.Text`
   margin-left: 20px;
 `;
 
-// const TrendingScroll = styled.FlatList`
-//   margin-top: 15px;
-// `;
-
 export default Movies;
+
+// 동일한 방향의 스크롤뷰가 있으면 안된다는 에러문구를 해결하기 위해
+// Container에 썼던 ScrollView를 제거하고 FlatList로 사용할것이다
+// 하지만 FlatList는 prop들의 묶음이기 때문에 자식요소를 가지지 않는다
+// 그래서 우리는 FlatList를 만들어서 Swiper를 render하고 Horizontal FlatList와 Vertical FlatList를 render 해야 한다
+
+// 그래서 이러한 문제를 해결하기 위해서 ListHeaderComponent prop를 사용하면 된다
+// ListHeaderComponent는 컴포넌트들을 모든 아이템 위쪽으로 render해버린다
+
+// 우선 세로로 만든 리스트들을 FlatList로 만든다
